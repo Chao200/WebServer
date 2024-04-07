0f0b990f87c34b78e7c6dea7e572bdf7afd31aad
chao <xiaochao200@126.com>
2024-04-07 20:59:26 +0800

    add comment

--- CGImysql/sql_connection_pool.h
+++ CGImysql/sql_connection_pool.h
@@ -14,45 +14,49 @@
 
 using namespace std;
 
+/* 数据库连接池 */
 class connection_pool
 {
 public:
-	MYSQL *GetConnection();				 //获取数据库连接
-	bool ReleaseConnection(MYSQL *conn); //释放连接
-	int GetFreeConn();					 //获取连接
-	void DestroyPool();					 //销毁所有连接
+	MYSQL *GetConnection();				 // 获取数据库连接
+	bool ReleaseConnection(MYSQL *conn); // 释放连接
+	int GetFreeConn();					 // 获取连接
+	void DestroyPool();					 // 销毁所有连接
 
 	// 单例模式
 	static connection_pool *GetInstance();
 
-	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log); 
+	// 初始化变量，数据库地址、用户名、密码、数据库名、端口号、最大连接数、是否关闭日志
+	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log);
 
 private:
 	connection_pool();
 	~connection_pool();
 
-	int m_MaxConn;  //最大连接数
-	int m_CurConn;  //当前已使用的连接数
-	int m_FreeConn; //当前空闲的连接数
+	int m_MaxConn;	// 所拥有的最大连接数
+	int m_CurConn;	// 当前已使用的连接数
+	int m_FreeConn; // 当前空闲的连接数
 	locker lock;
-	list<MYSQL *> connList; //连接池
-	sem reserve;
+	list<MYSQL *> connList; // 连接池，list 存储
+	sem reserve;	// 信号量，表示可用连接的资源数
 
 public:
-	string m_url;			 //主机地址
-	string m_Port;		 //数据库端口号
-	string m_User;		 //登陆数据库用户名
-	string m_PassWord;	 //登陆数据库密码
-	string m_DatabaseName; //使用数据库名
-	int m_close_log;	//日志开关
+	string m_url;		   // 主机地址
+	string m_Port;		   // 数据库端口号
+	string m_User;		   // 登陆数据库用户名
+	string m_PassWord;	   // 登陆数据库密码
+	string m_DatabaseName; // 使用数据库名
+	int m_close_log;	   // 日志开关
 };
 
-class connectionRAII{
+/* RAII */
+class connectionRAII
+{
 
 public:
 	connectionRAII(MYSQL **con, connection_pool *connPool);
 	~connectionRAII();
-	
+
 private:
 	MYSQL *conRAII;
 	connection_pool *poolRAII;
